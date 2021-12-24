076aadc07638272504fcbf7509fcaba75c200199
alex-laycalvert <alex.laycalvert@gmail.com>
2021-12-24 15:50:18 -0500

    Updated project name in code and Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
 
-use copy::Config;
+use copy_clone::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,7 +9,7 @@ fn main() {
         eprintln!("Error: {}", err);
         process::exit(1);
     });
-    if let Err(e) = copy::run(config) {
+    if let Err(e) = copy_clone::run(config) {
         eprintln!("Error: {}", e);
         process::exit(1);
     }
